8c14a14eb85d0ec5c74449a36a4b1d13587e4398
Gabbana237 <mefirehamed936@gmail.com>
2025-04-11 08:38:06 -0700

    Crud client-service

---
+++ client/src/main/java/com/microservice/client/ClientService.java
@@ -0,0 +1,34 @@
+package com.microservice.client.services;
+
+import com.microservice.client.entities.Client;
+import com.microservice.client.repositories.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    public Optional<Client> findById(Long id) {
+        return clientRepository.findById(id);
+    }
+
+    public Client save(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public void deleteById(Long id) {
+        clientRepository.deleteById(id);
+    }
+}
